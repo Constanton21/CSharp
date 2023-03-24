34560b86b44a8031dea4adc0d7501bab6833aabb
Constanton <natant621@gmail.com>
2023-03-24 13:34:05 +0300

    Задача3 четное и нечетное

---
+++ homework01/zadacha3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.Write($"число {a} четное");
+} else 
+{
+    Console.Write($"число {a} нечетное");
+}
