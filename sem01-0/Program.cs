7dd6b3da0ec6b7f61b7d820b8f049e8a3b005921
Constanton <natant621@gmail.com>
2023-03-24 11:10:41 +0300

    sem01-0 Возведение в степень

---
+++ sem01-0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sqr = a * a;
+Console.WriteLine($"Квадрат числа {a} равен {sqr}");
+int sqr1 = Convert.ToInt32(Math.Pow(a, 3));
+Console.WriteLine($"Куб числа {a} равен {sqr1}");
+
