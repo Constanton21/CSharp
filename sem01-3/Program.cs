fb5ffbf6082a9c857625441dc81f11ea492253d3
Constanton <natant621@gmail.com>
2023-03-24 11:42:53 +0300

    sem01-3 дни недели

---
+++ sem01-3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("Понедельник");
+}if (a == 2)
+{
+    Console.WriteLine("Вторник");
+}if (a == 3)
+{
+    Console.WriteLine("Среда");
+}if (a == 4)
+{
+    Console.WriteLine("Четверг");
+}if (a == 5)
+{
+    Console.WriteLine("Пятница");
+}if (a == 6)
+{
+    Console.WriteLine("Суббота");
+}if (a == 7)
+{
+    Console.WriteLine("Воскресенье");
+}if (a > 7) 
+{
+    Console.WriteLine("Нет такого дня недели");
+}if (a < 1) 
+{
+    Console.WriteLine("Нет такого дня недели");
+}
