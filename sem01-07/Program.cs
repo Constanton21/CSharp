6c6944e1217ff28b0c07fed415da73e18e923b70
Constanton <natant621@gmail.com>
2023-03-24 12:33:56 +0300

    sem01-07 поиск последней цифры в трехзначном

--- sem01-07/Program.cs
+++ sem01-07/Program.cs
@@ -1,4 +1,10 @@
 ﻿Console.Write("введите трехзначное число: ");
 int a = int.Parse(Console.ReadLine());
-int b = a%10;
-Console.WriteLine($"последняя цифра числа {a} будет {b}");
+
+while (a < 100 || a > 999)
+{   
+    Console.Write("введите трехзначное число: ");
+    a = int.Parse(Console.ReadLine()); 
+} 
+    int b = a % 10;
+    Console.WriteLine($"последняя цифра числа {a} будет {b}");
