6a5e123e6a1982b4cbecec439d408802680f3cbb
Constanton <natant621@gmail.com>
2023-03-24 11:22:58 +0300

    les01-1 квадрат ли A чису B

---
+++ sem01-1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine($"Да, {a} является квадратом {b}");
+}
+else 
+{
+    Console.WriteLine($"Нет, {a} не является квадратом {b}");
+}
