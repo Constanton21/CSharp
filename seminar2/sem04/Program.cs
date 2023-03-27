f4ae3f8f0344a556e857971df86707d04729e412
Constanton <natant621@gmail.com>
2023-03-27 14:21:09 +0300

    задача 4 проверка квадрата первого и второго значения

---
+++ seminar2/sem04/Program.cs
@@ -0,0 +1,19 @@
+﻿// команды для удобства
+//Console.Clear();
+//int a = new Random().Next(100, 999);
+//Console.Write($"{b} ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int a = int.Parse(Console.ReadLine());     ||   &&
+// if ((Convert.ToInt32(Math.Pow(a, 2)) == b) || (b * b == a))
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+ if ((Convert.ToInt32(Math.Pow(a, 2)) == b) || (b * b == a))
+ {
+    Console.Write($"да");
+ }else 
+{ 
+    Console.Write($"нет");
+}
+
