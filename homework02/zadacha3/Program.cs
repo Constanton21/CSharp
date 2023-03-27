f9d6400474f8eb55c780a1e1236bc11fa8d14097
Constanton <natant621@gmail.com>
2023-03-27 16:25:33 +0300

    Зачада 3 выходные и будничые дни

---
+++ homework02/zadacha3/Program.cs
@@ -0,0 +1,22 @@
+﻿// команды для удобства
+//Console.Clear();
+//int a = new Random().Next(100, 999);
+//Console.Write($"{b} ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int a = int.Parse(Console.ReadLine());     ||   &&
+// if ((Convert.ToInt32(Math.Pow(a, 2)) == b) || (b * b == a))
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine());
+
+if ((a < 1) || (a >7))
+{
+    Console.WriteLine("Неправильный номер дня недели, введите от 1 до 7");
+    a = int.Parse(Console.ReadLine());
+} if ((a == 6) || (a ==7)) {
+    Console.WriteLine("Выходной");
+} else {
+    Console.Write("Будничный день");
+}
+
