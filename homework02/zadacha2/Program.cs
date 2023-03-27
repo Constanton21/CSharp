2f28e8db222ae223c9356e15b4dfbb591412a21c
Constanton <natant621@gmail.com>
2023-03-27 16:28:27 +0300

    Зачада 2 не решенная пока

---
+++ homework02/zadacha2/Program.cs
@@ -0,0 +1,20 @@
+﻿// команды для удобства
+//Console.Clear();
+//int a = new Random().Next(100, 999);
+//Console.Write($"{b} ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int a = int.Parse(Console.ReadLine());     ||   &&
+// if ((Convert.ToInt32(Math.Pow(a, 2)) == b) || (b * b == a))
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+} else 
+{ 
+    int b = a % 1000;
+
+}
