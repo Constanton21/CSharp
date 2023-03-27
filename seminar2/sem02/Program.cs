eeaf37a050c05e1582a6703e0caa91715ed82eaa
Constanton <natant621@gmail.com>
2023-03-27 13:49:09 +0300

    поправлена немного2

--- seminar2/sem02/Program.cs
+++ seminar2/sem02/Program.cs
@@ -1,9 +1,10 @@
 ﻿// команды для удобства
 //Console.Clear();
 //int a = new Random().Next(100, 999);
-//Console.Write("+");
 //Console.Write($"{b} ");
-int b = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// int a = int.Parse(Console.ReadLine());
+
 
 int a = int.Parse(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
