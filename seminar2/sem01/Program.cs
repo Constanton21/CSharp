c243a0c819d2067ec99242d164ffa72b467ea5dc
Constanton <natant621@gmail.com>
2023-03-27 13:21:12 +0300

    первая задача первый и третий знак трехзначного числа

---
+++ seminar2/sem01/Program.cs
@@ -0,0 +1,12 @@
+﻿//Console.Clear();
+//int a = new Random().Next(100, 999);
+//Console.Write("+");
+//Console.Write($"{b} ");
+
+Console.Clear();
+int a = new Random().Next(100, 999);
+Console.WriteLine(a);
+int b = a % 10;
+int c = a/100;
+Console.Write(c);
+Console.Write(b);
