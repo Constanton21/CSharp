68bb1896f547cfd2bfa8a5126789cc978c1089ff
Constanton <natant621@gmail.com>
2023-03-27 14:00:18 +0300

    задача 3 проверка на кратность 7 и 23

---
+++ seminar2/sem03/Program.cs
@@ -0,0 +1,15 @@
+﻿// команды для удобства
+//Console.Clear();
+//int a = new Random().Next(100, 999);
+//Console.Write($"{b} ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int a = int.Parse(Console.ReadLine());     ||   &&
+
+int a = int.Parse(Console.ReadLine());
+ if ((a % 7 == 0) && (a % 23 == 0))
+ {
+    Console.Write($"{a} кратно 7 и 23");
+ }else 
+{ 
+    Console.Write($"{a} не кратно одновременно 7 и 23");
+}
