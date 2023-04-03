620fe5ec614b84f8ac3e3d574fa883dade106ed4
Constanton <natant621@gmail.com>
2023-04-03 10:12:40 +0300

    Задача 3 куб значений всех числе от 0 до N

---
+++ homework03/zadacha3/Program.cs
@@ -0,0 +1,12 @@
+﻿// команды для удобства
+//Console.Clear();
+//int a = new Random().Next(100, 999);
+//Console.Write($"{b} ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int a = int.Parse(Console.ReadLine());     ||   &&
+// if ((Convert.ToInt32(Math.Pow(a, 2)) == b) || (b * b == a))
+Console.Clear();
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+for (int i = 0; i <= N; i++)
+Console.Write($"{Math.Pow(i, 3)} ");
