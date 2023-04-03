55c8120e7af516863f3578a03b29dee9557c2a83
Constanton <natant621@gmail.com>
2023-04-03 16:41:50 +0300

    задача 1 А в стапенеи В

---
+++ homework04/zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()); //вводимое число
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()); //вводимое число
+int res = 1;
+Console.WriteLine($"{Math.Pow(a, b)}");   //для проверки выполнения
+Console.WriteLine($"{a} в степени {b} -> {ApowB(res)}");
+
+int ApowB(int res){
+  while (b > 0){
+    res = res * a;
+    b--;
+      }
+  return res;
+}
