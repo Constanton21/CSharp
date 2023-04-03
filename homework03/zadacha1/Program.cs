26c2105209501fc377ead8e8bfcb22ae18debaec
Constanton <natant621@gmail.com>
2023-04-03 12:58:19 +0300

    Задача 1 палиндром

---
+++ homework03/zadacha1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+int N = int.Parse(Console.ReadLine());
+int n5 = N%10;
+int N0 = N/10;
+int n4 = N0%10;
+N0 = N0/10;
+int n3 = N0%10;
+N0 = N0/10;
+int n2 = N0%10;
+N0 = N0/10;
+int n1 = N0%10;
+if (n1 == n5 && n2 == n4)
+{
+    Console.WriteLine($"Да, число {N} является палиндромом ");
+} else Console.WriteLine($"Нет, число {N} не палиндром ");
+
