06c6a8ae87fd43659bf9b86f49cb7643329e1c3b
Constanton <natant621@gmail.com>
2023-04-03 16:02:23 +0300

    задача 2 сумма цифр числа

---
+++ homework04/zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число n: ");
+int n1 = int.Parse(Console.ReadLine()); //вводимое число
+int res = 0;// сумма
+int n = n1;//меняющееся значение
+Console.WriteLine($"Сумма чисел числа {n1} -> {SumCifr(res)}");
+
+
+int SumCifr(int res){
+  while (n >= 1){
+    int i = n%10;
+    res =res+i;
+    n = n/10;
+  }
+  return res;
+}
