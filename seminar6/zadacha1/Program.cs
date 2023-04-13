07817c45dca8649d6d01c70a33f7fba7274ec011
Constanton <natant621@gmail.com>
2023-04-13 16:08:37 +0300

    6 seminar done

---
+++ seminar6/zadacha1/Program.cs
@@ -0,0 +1,22 @@
+﻿// **Задача 40:**Напишите программу,
+// которая принимает на вход три числа и проверяет, может ли
+// существовать треугольник с сторонами такой длины.
+
+Console.Clear();
+
+Console.Write("Введите длины сторон треугольника через пробел: ");
+
+string[] str = Console.ReadLine().Split(' ');
+int a = int.Parse(str[0]);
+int b = int.Parse(str[1]);
+int c = int.Parse(str[2]);
+
+if (IsTriangle(a, b, c)){
+    Console.Write("Yes");
+} else {
+    Console.Write("No");
+}
+
+bool IsTriangle(int a, int b, int c){
+return (((a + b) > c) && ((a + c) > b) && ((b + c) > a));
+}
---
+++ seminar6/zadacha2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(arrayToNull(num));
+
+// int arrayToNull(int numer)
+// {
+// List<int> mass = new List<int>();
+// while (numer > 1)
+// {
+// mass.Add(numer % 2);
+// numer = numer / 2;
+// }
+// mass.Add(1);
+// mass.Reverse();
+// return (int.Parse(String.Join("", mass)));
+// }
+
+Console.Clear();
+int number = int.Parse(Console.ReadLine());
+string res = DecToNum(number, 2);
+Console.WriteLine($"{number} -> {res}");
+
+string DecToNum(int decNumber, int otherSystem)
+{
+string res = "";
+string nums = "0123456789ABCDEF";
+while (decNumber > 0)
+{
+
+res = nums[decNumber%otherSystem] + res;
+decNumber /= otherSystem;
+}
+return res;
+}
