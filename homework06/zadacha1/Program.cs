09ad2b5854375074cb79d55e4ecbf48fd302d99c
Constanton <natant621@gmail.com>
2023-04-13 21:34:46 +0300

    6 domashka done

---
+++ homework06/zadacha1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите числа от -100 до 100 через пробел: ");
+string[] str = Console.ReadLine().Split(' ');
+int[] array = GetArrayFromString(str);
+int count = 0;
+int N = CountPositiv(array, count);
+int m = str.Length;
+Console.Write($"из {m} введенных чисел, положительных чисел -> {N} ");
+
+int[] GetArrayFromString(string[] str){
+    int[] res = new int[str.Length];
+    for (int i=0; i < str.Length; i++){
+        res[i] = int.Parse(str[i]);
+    } return res;
+}
+
+int CountPositiv(int[] array, int count) {
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {count++;
+    }
+} return count;
+}
---
+++ homework06/zadacha2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.WriteLine("Введите число k1: ");
+double k1 = double.Parse(Console.ReadLine()!); //вводимое число
+Console.WriteLine("Введите число b1: ");
+double b1 = double.Parse(Console.ReadLine()!); //вводимое число
+Console.WriteLine("Введите число k2: ");
+double k2 = double.Parse(Console.ReadLine()!); //вводимое число
+Console.WriteLine("Введите число b2: ");
+double b2 = double.Parse(Console.ReadLine()!); //вводимое число
+
+
+double x = 0;
+double y = 0;
+x = FindX(x);
+y = FindY(y);
+Console.Write($"Точка пересечения двух прямых ({x}; {y})");
+
+double FindX(double x){
+    x = (b2-b1)/(k1-k2);
+return x;
+}
+
+double FindY(double y) {
+    y = (k1 * x) + b1;
+return y;
+}
